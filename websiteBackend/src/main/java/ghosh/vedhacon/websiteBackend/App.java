bcb64b5dc61e85cb428abd46547a803bde968058
Anurag Ghosh <anuraghosh.1992@gmail.com>
2018-01-14 11:30:12 +0530

    Done with initial setup of vedhacon webstie

---
+++ websiteBackend/src/main/java/ghosh/vedhacon/websiteBackend/App.java
@@ -0,0 +1,13 @@
+package ghosh.vedhacon.websiteBackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
